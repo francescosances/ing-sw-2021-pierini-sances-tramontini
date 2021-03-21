c0afa70e7179c51d39197855622b78c12f61e29f
Matteo Pierini <matteopierini999@gmail.com>
2021-03-21 19:01:17 +0100

    Implemented strongbox and warehouse

--- IntelliJ/src/main/java/it/polimi/ingsw/Model/Strongbox.java
+++ IntelliJ/src/main/java/it/polimi/ingsw/Model/Strongbox.java
@@ -4,16 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Strongbox implements Storage{
-    private Map<ResourceType, Integer> resources;
+    private final Map<ResourceType, Integer> resources;
 
     public Strongbox(){
         resources = new HashMap<ResourceType, Integer>();
     }
 
-    public void addResources(Map<ResourceType, Integer> resources){}
+    public void addResources(Map<ResourceType, Integer> resources){
+        resources.forEach((res, num) -> this.resources.put(res, this.resources.containsKey(res) ? this.resources.get(res) + num : num));
+    }
 
     @Override
-    public void removeResources(Map<ResourceType, Integer> resources){}
+    public void removeResources(Map<ResourceType, Integer> resources){
+        resources.forEach((res, num) -> this.resources.put(res, this.resources.get(res) - num));
+    }
 
     public int getResourcesNum(ResourceType resource){
         return this.resources.get(resource);
--- IntelliJ/src/main/java/it/polimi/ingsw/Model/Warehouse.java
+++ IntelliJ/src/main/java/it/polimi/ingsw/Model/Warehouse.java
@@ -15,17 +15,29 @@ public class Warehouse implements Storage{
         toBeStored = new ArrayList<Resource>();
     }
 
-public void addResource(int depotNumber, ResourceType res, int num) {
-}
+    public void addResource(int depotNumber, ResourceType res, int num) {
+        for (int i = 0; i < num; i++)
+            depots.get(depotNumber).addResource(res);
+    }
 
-@Override
-public void removeResources(Map<ResourceType, Integer> resources) {
-}
+    @Override
+    public void removeResources(Map<ResourceType, Integer> resources) {
+            resources.forEach((res, num) -> {
+                int i = num;
+                for (Depot dep : depots)
+                    while (dep.getOccupied() > 0 && dep.getResourceType() == res && i > 0){
+                        dep.removeResource();
+                        i--;
+                    }
+            });
+    }
 
-public void toBeStored(Resource[] resources) {
-}
+    public void toBeStored(Resource[] resources) {
+        for (Resource res : resources)
+            toBeStored.add((ResourceType) res);
+    }
 
-public void swapDepots(int first, int second) {
+    public void swapDepots(int first, int second) {
         if (depots.get(first).getOccupied() > depots.get(second).getSize() || depots.get(second).getOccupied() > depots.get(first).getSize())
             throw new IllegalArgumentException("Unable to swap selected depots");
 
@@ -42,14 +54,14 @@ public void swapDepots(int first, int second) {
             depots.get(first).addResource(secondResourceType);
         for (int i = 0; i < firstOccupied; i++)
             depots.get(second).addResource(firstResourceType);
-}
+    }
 
-public boolean hasResourcesToStore() {
-        return !this.toBeStored.isEmpty();
-}
+    public boolean hasResourcesToStore() {
+        return !toBeStored.isEmpty();
+    }
 
-public void popResourceToBeStored() {
-}
-
+    public Resource popResourceToBeStored(){
+        return toBeStored.remove(toBeStored.size() - 1);
+    }
 
 }
