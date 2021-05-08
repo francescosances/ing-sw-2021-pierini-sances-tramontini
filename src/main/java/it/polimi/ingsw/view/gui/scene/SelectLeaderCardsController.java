0fa5e2793ff3797ebe88704428753864afe79d96
Marco Tramontini <marco.tramontini@mail.polimi.it>
2021-05-08 18:10:15 +0200

    leader cards choice implemented

--- src/main/java/it/polimi/ingsw/view/gui/scene/SelectLeaderCardsController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/SelectLeaderCardsController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SelectLeaderCardsController extends Controller{
@@ -30,16 +30,13 @@ public class SelectLeaderCardsController extends Controller{
     @FXML
     protected Button btnChoose;
 
-    @FXML
-    public void choose(){
-
-    }
-
     private List<LeaderCard> leaderCardList;
 
-    private List<Boolean> cardsStatus;
+    private List<Boolean> cardsSelected = new ArrayList<>();
 
     private int cardsToChoose;
+
+    private int numCardsSelected = 0;
 
     @FXML
     public void initialize(List<LeaderCard> leaderCardList, int cardsToChoose) {
@@ -48,10 +45,43 @@ public class SelectLeaderCardsController extends Controller{
         lbl.setText("Choose "+cardsToChoose+" cards:");
         ImageView[] imageViews = {img1,img2,img3,img4};
         for(int i=0;i<leaderCardList.size();i++){
-            imageViews[i].setImage(new Image("../images/cards/"+leaderCardList.get(i).getCardName()+".png"));
+            cardsSelected.add(false);
+            imageViews[i].setImage(new Image("/images/cards/FRONT/"+leaderCardList.get(i).getCardName()+".png"));
+
+            final int index = i;
+            imageViews[index].setOnMouseClicked((e)->{
+                if(cardsSelected.get(index)){
+                    imageViews[index].getStyleClass().remove("card-selected");
+                    numCardsSelected--;
+                }else{
+                    if(numCardsSelected == cardsToChoose)
+                        return;
+                    imageViews[index].getStyleClass().add("card-selected");
+                    numCardsSelected++;
+                }
+                cardsSelected.set(index,!cardsSelected.get(index));
+                if(numCardsSelected == cardsToChoose){
+                    btnChoose.getStyleClass().remove("btn-disabled");
+                    btnChoose.getStyleClass().add("btn-active");
+                }else{
+                    btnChoose.getStyleClass().add("btn-disabled");
+                    btnChoose.getStyleClass().remove("btn-active");
+                }
+            });
         }
     }
 
-
+    @FXML
+    public void choose(){
+        if(numCardsSelected != cardsToChoose)
+            return;
+        LeaderCard[] cardsChosen = new LeaderCard[cardsToChoose];
+        int added =0;
+        for(int i=0;i<leaderCardList.size();i++){
+            if(cardsSelected.get(i))
+                cardsChosen[added++] = leaderCardList.get(i);
+        }
+        clientController.leaderCardsChoice(cardsChosen);
+    }
 
 }
