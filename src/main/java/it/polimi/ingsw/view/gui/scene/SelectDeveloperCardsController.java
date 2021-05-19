a36ba19d5d532b96c8a5776f24a2ac778f9aa587
Marco Tramontini <marco.tramontini@mail.polimi.it>
2021-05-19 21:14:01 +0200

    Development cards images rendering

--- src/main/java/it/polimi/ingsw/view/gui/scene/SelectDeveloperCardsController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/SelectDeveloperCardsController.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.model.PlayerBoard;
 import it.polimi.ingsw.model.cards.Deck;
 import it.polimi.ingsw.model.cards.DevelopmentCard;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.util.List;
@@ -14,8 +15,17 @@ public class SelectDeveloperCardsController extends Controller{
     protected ImageView card0_0,card1_0,card2_0,card3_0,card4_0,card5_0,card6_0,card7_0,card8_0,card9_0,card10_0,card11_0;
 
     @FXML
-    public void initialize(List<Deck<DevelopmentCard>> developmentCardList, int cardsToChoose, PlayerBoard userBoard){
+    public void initialize(List<Deck<DevelopmentCard>> developmentCardList, int cardsToChoose, PlayerBoard playerBoard){
+        ImageView[][] imgs = {{card0_0},{card1_0},{card2_0},{card3_0},{card4_0},{card5_0},{card6_0},{card7_0},{card8_0},{card9_0},{card10_0},{card11_0}};
 
+        for(int i=0;i<developmentCardList.size();i++){
+            DevelopmentCard topCard = developmentCardList.get(i).top();
+            imgs[i][0].setImage(new Image("/images/cards/FRONT/"+topCard.getCardName()+".png"));
+            if(playerBoard.acceptsDevelopmentCard(topCard) && topCard.getCost().satisfied(playerBoard)) {
+                imgs[i][0].getStyleClass().add("selectable");
+            }else
+                imgs[i][0].getStyleClass().remove("selectable");
+        }
     }
 
 }
